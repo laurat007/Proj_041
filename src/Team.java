83899031a5bbe8ab6efe5e361cd05f0334371ad9
laurat007 <lauratdn_7984@yahoo.ro>
2017-10-27 14:24:20 +0300

    first commit

---
+++ src/Team.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author laura
+ */
+public class Team {
+    double primarySkill;
+    double secondarySkill;
+    double genderMix;
+    int teamVelocity;
+    
+    
+}
---
+++ src/source.java
@@ -0,0 +1,27 @@
+
+import java.util.Hashtable;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author laura
+ */
+public class source {
+    public static void main(String arg[]) {
+        Hashtable ability = new Hashtable();
+        ability.put("FRONTEND", 1);
+        ability.put("BACKEND", 2);
+        ability.put("INTEGRATIONS", 3);
+        ability.put("AI", 4);
+        
+        String[] girlsName = {"ANA", "MARIA", "IOANA", "CORNELUSA", "BRANDUSA"};
+        String[] boysName = {"MIKE", "TAYSON", "JOHN", "CENA", "ROBY"};
+        
+        
+    }
+}
